433c92a1ba525add50781ac1a0978044836c29e2
Anomwarbhuvan <anom.21910758@viit.ac.in>
2022-08-21 13:39:24 +0530

    Checkout-Page-Updated

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,48 @@
+import './checkout.styles.scss';
+
+import { useContext } from 'react';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
+import { CartContext } from '../../context/cart.context';
+import CartItem from '../../components/cart-item/cart-item.component';
+
+const Checkout = () => {
+    const { cartItems , cartTotal} = useContext(CartContext);
+
+    return (
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+
+                </div>
+                <div className='header-block'>
+                <span>Description</span>
+
+                </div>
+                <div className='header-block'>
+                <span>Quantity</span>
+
+                </div>
+                <div className='header-block'>
+                <span>Price</span>
+
+                </div>
+                <div className='header-block'>
+                <span>Remove</span>
+
+                </div>
+            </div>
+            
+            {cartItems.map((cartItem) =>
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+                            
+                        
+            )}
+            <span className='Total'>Total : ${cartTotal}</span>
+         
+        </div>
+    );
+}; 
+ 
+export default Checkout;
