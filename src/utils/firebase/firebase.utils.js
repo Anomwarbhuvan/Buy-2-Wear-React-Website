7faa0333290d6e89e577e1740ca587b2bd68d0ee
Anomwarbhuvan <anom.21910758@viit.ac.in>
2022-08-24 11:45:52 +0530

    Finishednested routes

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword ,signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth';
 
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc ,collection, writeBatch,query,getDocs} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,17 +18,49 @@ const firebaseConfig = {
   // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
   
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleprovider = new GoogleAuthProvider();
+googleprovider.setCustomParameters({
     prompt: "select_account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider); 
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleprovider); 
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async(userAuth, additionalInformation) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsAdd ) => {
+ 
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+ 
+
+  objectsAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+
+  });
+
+  await batch.commit();
+  console.log('done');                                             
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  },{})
+
+  return categoryMap;
+}
+
+export const createUserDocumentFromAuth = async(userAuth, additionalInformation ={} ) => {
   if (!userAuth) return;
   
   const userDocRef = doc(db, 'users', userAuth.uid);
